7caf3203c2297ac7b6efe59434e0473ac3188e42
mdqaoud <mdqaoud@gmail.com>
2022-01-03 21:27:12 +0200

    Second Commmit

---
+++ app/src/main/java/com/example/bmiv2/Addfooddetails.java
@@ -0,0 +1,16 @@
+package com.example.bmiv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Addfooddetails extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addfooddetails);
+    }
+}
